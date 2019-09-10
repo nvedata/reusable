849c1433bdf59993df286dbf9560b3eb06218edc
Skilbrum <36617518+Skilbrum@users.noreply.github.com>
2019-09-10 16:38:20 +0300

    Add functions
    Add isnumber, convert_cyr_month, time_derivative
    Update excel_date

--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
+import numpy as np
+import pandas as pd
+import datetime
+
 def excel_date(date):
-    '''Converting date to Excel date or from Excel date
+    '''
+    Converting date to Excel date or from Excel date
     
     Parameters
     ----------
@@ -12,11 +17,74 @@ def excel_date(date):
     Excel date as float/int or date as pd.Timestamp.
     '''
     
+    #unlike python, 29.02.1900 exists in excel
+    #since 01.03.2019 pandas and excel calendar has equal dates
+    cons_date = pd.Timestamp(year=1900, month=3, day=1)
+    excel_cons_date = 61
+    
     if isinstance(date, (str, datetime.datetime, pd.Timestamp)):
-        return (pd.to_datetime(date)-pd.Timestamp(year=1899, month=12, day=31)).days+1
+        return (pd.to_datetime(date) - cons_date).days + excel_cons_date
     
     elif isinstance(date, (np.int, np.float)):
-        return pd.Timestamp(year=1899, month=12, day=31)+pd.Timedelta(days=date-1)
+        return cons_date + pd.Timedelta(days=date - excel_cons_date)
         
     else:
         raise TypeError('expected str, datetime, float, int, got ', type(date))
+
+def time_derivative(series, time_unit=pd.Timedelta('1s')):
+    '''
+    Calculate time derivative from right for each point in series. Ignores NaN.
+    
+    Parameters
+    ---------
+    series: pd.Series with numeric data with pd.DatetimeIndex
+    time_unit: pd.Timedelta
+    
+    Returns
+    ---------
+    dsdt : pd.Series
+    '''
+    
+    ds=series.dropna().diff()
+    dt=ds.index.to_series().diff()
+    dsdt=ds/(dt/time_unit)
+    return dsdt
+
+def isnumber(a):
+    try:
+        float(a)
+        return True
+    except:
+        return False
+    
+def convert_cyr_month(series):
+    '''
+    Parameters
+    ----------
+    series: pd.Series of str
+    
+    Returns
+    ----------
+    series: pd.Series
+    '''
+    series=series.copy()
+    
+    repl_dict={
+    'янв\\w*' : '01',    
+    'фев\\w*' : '02',    
+    'мар\\w*' : '03',
+    'апр\\w*' : '04',
+    'май\\w*' : '05',
+    'июн\\w*' : '06',
+    'июл\\w*' : '07',
+    'авг\\w*' : '08',
+    'сен\\w*' : '09',
+    'окт\\w*' : '10',
+    'ноя\\w*' : '11',
+    'дек\\w*' : '12'
+    }
+    
+    for k, v in repl_dict.items():
+        series=series.str.replace(k, v)
+    
+    return series
